d02e7c747bbe82b297e53691ecd70f317ae5a9f4
peryaudo <peryaudo@gmail.com>
2021-03-28 00:00:19 +0900

    Fill metadata

--- main.go
+++ main.go
@@ -6,8 +6,11 @@ import (
 	"log"
 	"net/http"
 	"syscall"
+	"os"
+	"time"
 
 	"github.com/hanwen/go-fuse/fs"
+	"github.com/hanwen/go-fuse/fuse"
 )
 
 type EntryKind int
@@ -34,16 +37,49 @@ func (k *EntryKind) UnmarshalJSON(b []byte) error {
 type JsonEntry struct {
 	Kind    EntryKind   `json:"kind"`
 	Name    string      `json:"name"`
+	Size	uint64 `json:"size"`
+	LastModified uint64 `json:"lastModified"`
 	Entries []JsonEntry `json:"entries"`
+}
+
+type dirNode struct {
+	metadata *JsonEntry
+	fs.Inode
+}
+
+func (dn *dirNode) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	out.Mode = 0755
+	out.Uid = uint32(os.Getuid())
+	out.Gid = uint32(os.Getgid())
+	return 0
+}
+
+type fileNode struct {
+	metadata *JsonEntry
+	fs.Inode
+}
+
+func (fn *fileNode) Getattr(ctx context.Context, f fs.FileHandle, out *fuse.AttrOut) syscall.Errno {
+	out.Mode = 0644
+	out.Uid = uint32(os.Getuid())
+	out.Gid = uint32(os.Getgid())
+	out.Atime = fn.metadata.LastModified
+	out.Mtime = fn.metadata.LastModified
+	out.Ctime = fn.metadata.LastModified
+	out.Size = fn.metadata.Size
+	const bs = 512
+	// out.Blksize = bs
+	out.Blocks = (out.Size + bs - 1) / bs
+	return 0
 }
 
 func addEntry(parent *fs.Inode, entry *JsonEntry) {
 	var child *fs.Inode
 	switch entry.Kind {
 	case File:
-		child = parent.NewPersistentInode(context.Background(), &fs.Inode{}, fs.StableAttr{})
+		child = parent.NewPersistentInode(context.Background(), &fileNode{metadata: entry}, fs.StableAttr{})
 	case Directory:
-		child = parent.NewPersistentInode(context.Background(), &fs.Inode{}, fs.StableAttr{Mode: syscall.S_IFDIR})
+		child = parent.NewPersistentInode(context.Background(), &dirNode{metadata: entry}, fs.StableAttr{Mode: syscall.S_IFDIR})
 		for _, subEntry := range entry.Entries {
 			addEntry(child, &subEntry)
 		}
@@ -52,6 +88,7 @@ func addEntry(parent *fs.Inode, entry *JsonEntry) {
 }
 
 func main() {
+	bootTime = time.Now().Unix()
 	root := &fs.Inode{}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
